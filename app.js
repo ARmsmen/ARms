78314768de5d63ba1ba755d80a61037ed9831a25
shchau <31778426+shchau@users.noreply.github.com>
2018-01-27 19:57:31 -0700

    Update app.js

--- app.js
+++ app.js
@@ -101,12 +101,6 @@ var Vec3 = function(){
 }
 
 
-/*function update(){
-	for (var i in SOCKET_LIST){
-		SOCKET_LIST[i].emit('serverInfo', PLAYER_LIST);
-	}
-}
-*/
 
 var socketList = {};
 var playerList = {}; //player list of Player-type variables
@@ -204,96 +198,4 @@ io.sockets.on('connection', function(socket){
 	
 	}
 }
-
-var Vec3 = function(){
-	return {x:0, y:0, z:0}	
-}
-
-
-/*function update(){
-	for (var i in SOCKET_LIST){
-		SOCKET_LIST[i].emit('serverInfo', PLAYER_LIST);
-	}
-}
-*/
-
-var socketList = {};
-var playerList = {}; //player list of Player-type variables
-
-io.sockets.on('connection', function(socket){
-	
-	socket.id = makeId();
-	
-	socketList[socket.id] = socket; 
-	var player
-	
-	socket.on('createRoom', function(data) {
-		var room = Room.make();
-		player = Player.make();
-		room.addPlayer(player);
-		socket.emit('createRoomSuccess', 
-		{	player : player.getInfo(), 
-			room : room.getInfo()
-		});
-											
-		roomList.addRoom(room);
-	});
-	
-	playerList[socket.id] = player;
-	
-	
-	socket.on('buttonPress', function(msg){
-		console.log(msg.data);
-		console.log("Message received from Client");
-	});
-	
-	
-	function makeId()
-	{
-		var id = Math.random().toString().slice(-7,-1);
-		while (socketList[id]) id = Math.random().toString().slice(-7,-1);
-		return id;
-	}
-	
-	socket.emit('serverMsg', {number:5});
-	
-		
-	});
-	
-	/////////////////////////////////////////////////////////////////////////////
-	
-
-	var Player = {
-		
-		
-		make : function()
-		{ 
-			var self = {
-				direction : Vec3(),
-				id: ID.playerID.get(),
-				getInfo : Player.getInfo
-			}
-			return self;
-		},
-	
-		getInfo: function()
-		{
-			return {
-				id: this.id,
-				direction: this.direction
-			}
-		
-		},
-	
-		setInfo: function(data)
-		{
-			this.direction = data.direction 
-			//right now, all we need is the direction from the data being received from the clients
-		}
-	
-	}
-
-
-	
-	
 	
